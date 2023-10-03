8859902292ab4bdef326469d48721545ee828c4f
ArsenAzatkhanyan <arsenazatkhanyan2004@gmail.com>
2023-10-04 00:04:06 +0400

    'version1'

---
+++ python.py
@@ -0,0 +1,456 @@
+# 1
+
+# name = "Arsen"
+# pos = 5555
+# print(name)
+# print(pos)
+
+#2
+
+# name = input("Write your name:  ")
+# print("Hello " + name)
+
+#3
+
+# erk = float(input("Greq erkarutyuny: "))
+# layn = float(input("Greq laynutyuny: "))
+# hasiv = erk * layn
+# print(hasiv)
+
+#4
+
+# erk = float(input("Greq erkarutyuny: "))
+# layn = float(input("Greq laynutyuny: "))
+# akr = 1897473600
+# print(erk * layn / akr)
+
+
+#5
+
+# poqr = 0.25
+# mec = 0.50
+# dlr = " $"
+# p = int(input("How many bottles you have:  "))
+# m = int(input("How many big bottles you have:  "))
+# hashiv = poqr * p
+# hashiv2 = mec * m
+# print(hashiv)
+# print(hashiv2)
+
+#6
+
+
+
+
+# x = input("Chose Phone model:  ")
+# y = input("Chose a color:  ")
+# print(x == "iphone" and y == "Gold")
+
+
+# x1 = input("vorn e Hayastani mayraqaxaqy:  ")
+# x2 = input("vorn e Fransiayi mayraqaxaqy:  ")
+# x3 = input("vorn e Rusastani mayraqaxaqy:  ")
+# x4 = input("vorn e Amerikai mayraqaxaqy:  ")
+# x5 = input("ov e stexcel iphony:  ")
+# print(x1 == "Erevan", x2 == "Paris", x3 == "Moskva", x4 == "Vashington", x5 == "Steve Jobs")
+
+
+
+
+# tariq = 24
+# g = input("Greq Dzer anuny:  ")
+# t = int(input("Greq Dzer Tariqy: "))
+# print(t >= 15 and t <= 26)
+
+# car = input("Chose car model:  ")
+# color = input("Chose car color:  ")
+# data = int(input("Chose car data:  "))
+# print(car == "BMW" and color == "Black" and data == 202
+
+
+
+# a = 5 
+# b = 6
+# x = (a + b) ** 3  
+# print(x)
+
+
+# import random 
+# x = 5 
+# print(random.random(0, 10))
+
+
+# s = 20
+# q = 657
+# x = q / s
+# print(x, "Sigarets you have")
+
+
+#14
+
+
+
+
+
+# x = float(input("Write futs:  "))
+# y = float(input("Write dyum:  "))
+# z = 30.48
+# c = 2.54
+# q = x / z
+# w = y / c
+# print(q, w)
+
+# a = input("yntreq guyn: ")
+# x = "kaput"
+# y = "kanach"
+# print(a == x, a == y )
+
+
+# x = 500
+# d = 500
+# print(x * 4, d * 5)
+
+# a = int(input("greq qaranish tiv:  "))
+# print((a%10)*1000+(a//10%10)*100+(a//100%10)*10+a//1000%10)
+
+#35
+
+# tiv = int(input("Greq tiv: "))
+# if tiv % 2 == 0:
+#     print("zuyg")
+# else:
+#     print("Kent")
+
+#37
+# tar = input("Greq tar: ")
+# x = ("a", "e", "i", "o", "u")
+# if tar in x:
+#     print("glasnaya")
+# elif tar == "y":
+#     print("ham glasnoy ham soglasnoy")
+# else:
+#     print("soglasnaya")
+
+#38
+
+# figura = int(input("Greq ugleri qanaky: "))
+# if figura == 3:
+#     print("erankyun")
+# elif figura == 4:
+#     print("qarankyun")
+# elif figura == 5:
+#     print("hngankyun")
+# elif figura == 6:
+#     print("vecankyun")
+# elif figura == 7:
+#     print("yotankyun")
+# elif figura == 8:
+#     print("utankyun")
+# elif figura == 9:
+#     print("inankyun")
+# elif figura == 10:
+#     print("tasankyun")
+# else:
+#     print("grvac tivy chi hamapatasxanum:")
+
+
+# x = int(input("Write your age:  "))
+# y = input("Male or Female: ")
+# if x == 18:
+#     print("nice")
+# elif x == 19:
+#     print("nice")
+# elif x == 20:
+#     print("nice")
+# else:
+#     print("poqr es")
+# if y == "male":
+#     print("ancar axper")
+# else:
+#     print("chanchar")
+
+# tiv = float(input("Write a number: "))
+# tiv2 = float(input("Write second number: "))
+# tiv3 = float(input("Write 3th number: "))
+# tiv4 = float(input("Write 4th number: "))
+# tiv5 = float(input("Write 5th number: "))
+# hashvark = tiv + tiv2 + tiv3 + tiv4 + tiv5
+# print(hashvark)
+
+# erk = 1
+# ere = 2
+# cho = 3
+# hin = 4
+# urb = 5
+# shb = 6
+# krk = 7
+# harc = int(input("greq 1 - 7 tveric meky: "))
+# if harc == erk:
+#     print("Erkushabti: 1")
+# elif harc == ere:
+#     print("Ereqshabti: 2")
+# elif harc == cho:
+#     print("Choreqshabti: 3")
+# elif harc == hin:
+#     print("Hingshabti: 4")
+# elif harc == urb:
+#     print("Urbat: 5")
+# elif harc == shb:
+#     print("Shabat: 6")
+# elif harc == krk:
+#     print("Kiraki: 7")
+# else:
+#     print("Dzer mutqagrvac tivy sxal e: ")
+
+
+# a = "Shnor*havor"
+# x = "*"
+# for i in range(0, len(a)):
+#     if i == x:
+# print(len(x))
+
+# harc = int(input("Greq sharqeri qanaky: "))
+# harc2 = int(input("Greq atorneri qanaky: "))
+# harc3 = int(input("Greq sharqeri heravorutyuny(metr): "))
+# sharq = "|\n"
+# ator = "="
+# metr = "*"
+# h1 = harc * sharq
+# h2 = harc2 * ator
+# h3 = harc3 * metr
+# print(h1, h2, h3)
+
+#63
+
+# summ = 0
+# count = 0
+# while True:
+#     n = int(input("Enter the number: "))
+#     if n == 0:
+#         if count != 0:
+#             print(summ / count)
+#         else:
+#             print(0)
+#         break
+#     else:
+#         count += 1
+#         summ += n
+
+#z2
+
+# anun = input("Greq Dzer anuny: ")
+# while True:
+#     n = int(input("Greq gumari chapy: "))
+#     if n >= 1000:
+#         print(anun + " Duq fuflo cheq: ")
+#         break
+#     else:
+#         print(anun + " duq hly fuflo eq: ")
+    
+    
+#z3
+
+# num = int(input("Write a number: "))
+# print(len(str(num)))
+
+# num = int(input("Write a number: "))
+# while True:
+#     print(len(str(num)))
+#     break
+
+#4
+
+# polo = 0
+# otri = 0
+
+# while True:
+#     num = int(input("Write a number (Write 0 for stop a program): "))
+#     if num >= 1 <= 100:
+#         if num == 0:
+#             break
+#         else:
+#             num == 0
+#             break
+#     elif num >= -100 <= -1:
+# #         polo += 1
+# #         otri += 1
+        
+
+# # print("polojitelnie: " + (str)(polo))
+# # print("otricatelnie: " + (str)(otri))
+
+
+
+# #2
+
+# # txt = ['hello', 'world', 'you', 'are', 'beauty']
+# # print(txt[::-1])
+
+# #3
+
+# # txt = ['hello', 'world', 'you', 'are', 'beauty']
+# # print(len(txt))
+
+# #5
+# # num = (2, 96, 8,)
+# # x = num[0] + num[1] + num[2]
+# # print(x)
+
+# #3
+
+# # num = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+# # sim = ('!', '@', '#', '$', '%', '&', '*')
+# # password = input("Write a password: ")
+# # if password != sim:
+# #     print("Greq mek simvol: ")
+# # elif password < len(8):
+# #     print("Greq 8 kam aveli tarer: ")
+# # elif password != num:
+# #     print("Greq tiv: ")
+# # else:
+# #     print("Password have a simvols, text and numbers:")
+
+
+
+# #110
+# # mylist = []
+# # while True:
+# #     num = int(input("Write a number: "))
+# #     if num != 0:
+# #         mylist.append(num)
+# #     else:
+# #         mylist.sort()
+# #         break
+
+# # print(mylist)
+
+
+# #111
+# # mylist = []
+# # while True:
+# #     num = int(input("Write a number: "))
+# #     if num != 0:
+# #         mylist.append(num)
+# #     else:
+# #         break
+# # mylist.sort()
+# # mylist.reverse()
+# # print(mylist)
+
+# # mylist = []
+
+# # for i in range(3):
+# #     age = int(input("Write your age: "))
+# #     mylist.append(age)
+# #     if age < 16:
+# #         print("Too young!")
+# #     else:
+# #         print("Get ready!")
+# # print(mylist)
+
+# sim = "!@#$%^&*,./<>?\`~:'+-"
+# num = "1234567890"
+# txt = "QWERTYUIOPASDFGHJKLZXCVBNM"
+
+# while True:
+#     password = input("Write your password: ")
+#     if len(password) < 6:
+#         print("Your password lenght can have min 6")
+#     elif not txt in password:
+#         print("Write capital letter")
+#     elif not num in password:
+#         print("Write numbers ")     
+#     elif not sim in password:
+#         print("Write simvols")
+#     else:
+#         print("Your password not strong, write simvols, number and capital letter")
+# print("Your password strong")
+
+
+
+# count = {}
+
+# txt = input("Write text: ")
+# for i in txt:
+#     if i in count:
+#         count[i] +=1
+#     else:
+#         count[i] = 1
+# print(count)
+
+
+#173
+
+
+
+# def func():
+#     num = int(input("Write a number: "))
+#     if num == " ":
+#         return 0
+#     else:
+#         return num + func()
+
+# print(func())
+
+
+#174
+
+# def func():
+#     a = int(input("Enter a num: "))
+#     b = int(input("Enter second num: "))
+#     if b == 0:
+#         return a
+#     else:
+#         c = a % b
+
+# print(func())
+
+#175
+
+# def func(n):
+#     if n == 1:
+#         return "1"
+#     else:
+#         return func(n // 2) + str(n % 2)
+# print(func(43))
+
+# #176
+
+# table = {
+#     "A": "Alpha",
+#     "B": "Bravo",
+#     "C": "Charlie",
+#     "D": "Delta",
+#     "E": "Echo",
+#     "F": "Foxtrot",
+#     "G": "Golf",
+#     "H": "Hotel",
+#     "I": "India",
+#     "J": "Juliet",
+#     "K": "Kilo",
+#     "L": "Lima",
+#     "M": "Mike",
+#     "N": "November",
+#     "O": "Oscar",
+#     "P": "Papa",
+#     "Q": "Quebec",
+#     "R": "Romeo",
+#     "S": "Sierra",
+#     "T": "Tango",
+#     "U": "Uniform",
+#     "V": "Victor",
+#     "W": "Whiskey",
+#     "X": "Xray",
+#     "Y": "Yankee",
+#     "Z": "Zulu"
+# }
+
+
+# def func():
+#     txt = input("Write text: ").upper
+#     if txt == "":
+#         return ""
+
+
+# print(func())
+
